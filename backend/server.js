e3547aa65881c39e3afeb4fc220910e920869d23
Natalia Shport <shport.natalia@gmail.com>
2021-03-05 11:10:38 -0800

    Server port changed

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ if (process.env.NODE_ENV === "production") {
 }
 app.use(notFound)
 app.use(errorHandler)
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold))
