a6cc64b5d60b7ffdb874392521339b17ecf5bc90
Natalia Shport <shport.natalia@gmail.com>
2021-03-18 08:58:06 -0700

    Changed .env

--- backend/server.js
+++ backend/server.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
   app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")))
 } else {
   app.get("/", (req, res) => {
-    res.send("API is running....")
+    res.send("API server is running....")
   })
 }
 app.use(notFound)
