cb7b406b479716a72f2a539ae234500cd5b6a97a
Natalia Shport <shport.natalia@gmail.com>
2021-04-12 01:10:37 -0700

    Fixed adding localCart Items to newly registered User cart

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -32,23 +32,25 @@ export const registerUser = asyncHandler(async (req, res) => {
   if (userExists) {
     res.status(400)
     throw new Error("User already exists")
-  }
-  const user = await User.create({
-    name,
-    email,
-    password
-  })
-  if (user) {
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token: generateToken(user._id)
-    })
   } else {
-    res.status(400)
-    throw new Error("Invalid user data")
+    const user = await User.create({
+      name,
+      email,
+      password
+    })
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        storecredit: user.storecredit,
+        token: generateToken(user._id)
+      })
+    } else {
+      res.status(400)
+      throw new Error("Invalid user data")
+    }
   }
 })
 
